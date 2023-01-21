495b4b39e0dcad9d369fbe391da359cb88bf0a11
keshavcodex <keshavcodex@gmail.com>
2023-01-21 13:21:25 +0530

    added dropdown

---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import Dropdown from "./dropdown";
+function navbar(){
+
+    return(
+        <div>
+        <Dropdown />
+            <h1>nav bar</h1>
+        </div>
+    );
+
+}
+export default navbar;
